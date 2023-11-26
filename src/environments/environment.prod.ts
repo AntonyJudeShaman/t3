b1191f5c48b791508717e2b18825b9b356c50991
AntonyJudeShaman <antonyjudeshaman@gmail.com>
2023-11-26 10:49:43 +0530

    mm

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   supabaseUrl: 'https://fyocklcryihilwmejygz.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5b2NrbGNyeWloaWx3bWVqeWd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3OTgzNTMsImV4cCI6MjAxNjM3NDM1M30.5KSFatE9kPgULto5I1DUfiTIOltCl8sxFOxHlLQj9qM',
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   supabaseUrl: 'https://fyocklcryihilwmejygz.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5b2NrbGNyeWloaWx3bWVqeWd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3OTgzNTMsImV4cCI6MjAxNjM3NDM1M30.5KSFatE9kPgULto5I1DUfiTIOltCl8sxFOxHlLQj9qM',
 }
